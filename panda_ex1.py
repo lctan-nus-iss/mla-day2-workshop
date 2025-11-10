7caa6ec1c740889aef2b5ca0edbd89f637f7ec4d
Tan Lee Chiang <lctan@nus.edu.sg>
2025-11-10 14:08:19 +0800

    added panda workshop and products

---
+++ panda_ex1.py
@@ -0,0 +1,45 @@
+# Import pandas package
+# Load products.csv to a dataframe
+def load_products():
+    # Your code here
+    return None
+# Display first 5 rows
+def display_first_five_rows(df):
+    # Your code here
+    return None
+# Display last 5 rows
+def display_last_five_rows(df):
+    # Your code here
+    return None
+# Retrieve maximun unit price of the table
+def get_max_unit_price(df):
+    # Your code here
+    return None
+# Count the number of rows in the dataframe
+def count_rows(df):
+    # Your code here
+    return None
+# Count the number of columns in the dataframe
+def count_columns(df):
+    # Your code here
+    return None
+# Display the types of the columns
+def display_column_types(df):
+    # Your code here
+    return None
+# Display all columns for the 5th row (row 4)
+def display_fifth_row(df):
+    # Your code here
+    return None
+# Display columns 3 to 5 for the row 9
+def display_columns_3_to_5_row_9(df):
+    # Your code here
+    return None
+# Display row 3 to 5, columns from ProductName to UnitPrice
+def display_rows_3_to_5_columns_productname_to_unitprice(df):
+    # Your code here
+    return None
+# Display number of items (rows) for each category
+def count_items_per_category(df):
+    # Your code here
+    return None
